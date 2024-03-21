93721edd949a0e4659b4ff4061743494fe2fccd7
Alexander784 <alexandernyaga1999@gmail.com>
2024-03-21 12:10:42 +0300

    Add responsiveness to HeroBanner component

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -4,7 +4,11 @@ import { Box,Stack, Typography } from '@mui/material';
 
 const HeroBanner = () => {
   return (
-    <Box>
+    <Box sx={{
+        // Responsiveness
+        mt: { ig: "212px", xs: "70px"},
+        m1: { sm: "50px"}
+    }} position="relative" p="20px">
         <Typography color="#FF2625"
         fontweight="600" fontsize="26px">
             Fitness Club
