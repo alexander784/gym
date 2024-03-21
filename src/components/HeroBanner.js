8341250a161d03da9df893a6e48b806ca33362c7
Alexander784 <alexandernyaga1999@gmail.com>
2024-03-21 12:49:46 +0300

    Structure HerooBanner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Box,Stack, Typography } from '@mui/material';
-
+import { Box,Stack, Typography, Button } from '@mui/material';
+import banner from "../assets/banner.png";
 
 const HeroBanner = () => {
   return (
@@ -13,6 +13,18 @@ const HeroBanner = () => {
         fontweight="600" fontsize="26px">
             Fitness Club
         </Typography>
+        <Typography fontWeight={700}
+        // Responsiveness
+        sx={{ fontSize: {ig: "44px", xs: "40px" }}}>
+            Sweat, Smile <br/> and Repeat
+        </Typography>
+        <Typography fontSize="22px"
+        lineHeight="35px" mb={3}>
+            Check out most effective Exercises 
+        </Typography>
+        <Button variant="contained" color="error" href='#exercises'>Explore Exercises</Button>
+        <img src={banner} alt="Banner"
+        className='hero-banner-img'/>
 
     </Box>
   )
