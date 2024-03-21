1678120fa25d7dc49556f338c4865137c2a36c77
Alexander784 <alexandernyaga1999@gmail.com>
2024-03-21 10:46:14 +0300

    BAsic Layout of files and folders

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import {Route, Routes } from "react-router-dom";
-import { Box } from "@mui.material";
+import {
+  BrowserRouter as Router,
+  Route, Routes
+} from 'react-router-dom';
+import { Box } from "@mui/material";
 import "./App.css";
+import ExerciseDetail from "./pages/ExerciseDetails";
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+
+
 
 const App = () => {
   return (
-    <Box width="400px">
-        Navbar
+    <Box width="400px" sx={{ width: {x1: '1488px'}}} m="auto">
+        <NavBar />
+        <Router>
         <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/exercise/:id" element={<ExerciseDetail />}/>
-            
+
 
         </Routes>
+        </Router>
+        <Footer />
     </Box>
   )
 }
