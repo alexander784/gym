ec5cc8919e10456628cc1a81966fffa8a6f1a335
Alexander784 <alexandernyaga1999@gmail.com>
2024-03-22 09:02:30 +0300

    Structuring SearchExercises

--- src/components/SearchExercise.js
+++ src/components/SearchExercise.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Box, Stack, TextField, Typography } from '@mui/material';
 
 const SearchExercise = () => {
   return (
-    <div>SearchExercise</div>
+    // Wrap the section with a Stack component
+    <Stack alignItems="center" mt="37px"
+    justifyContent="center" p="20px">
+      <Typography fontWeight={700}
+
+      // Responsiveness
+      sx={{fontSize:{ ig: "44px", xs:"30px"}}}
+      mb="50px" textAlign="center">
+        Awesome Exercise you <br />
+        Should Know
+
+      </Typography>
+      <Box position="relative" mb ="720px">
+        <TextField 
+        height="76px"
+        value=""
+        onchange={ () => {}}
+        placeholder='Search Exercises'
+        type="text"/>
+
+      </Box>
+
+    </Stack>
   )
 }
 
