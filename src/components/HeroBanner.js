a53723ffdc4e9527c4af7b16bf3356e624798e62
Alexander784 <alexandernyaga1999@gmail.com>
2024-03-22 08:51:45 +0300

    Style Home page

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -15,14 +15,25 @@ const HeroBanner = () => {
         </Typography>
         <Typography fontWeight={700}
         // Responsiveness
-        sx={{ fontSize: {ig: "44px", xs: "40px" }}}>
+        sx={{ fontSze: {ig: "44px", xs: "40px" }}}
+        mb="23px" mt="30px">
             Sweat, Smile <br/> and Repeat
         </Typography>
         <Typography fontSize="22px"
-        lineHeight="35px" mb={3}>
+        lineHeight="35px" mb={4}>
             Check out most effective Exercises 
         </Typography>
-        <Button variant="contained" color="error" href='#exercises'>Explore Exercises</Button>
+        <Button variant="contained" color="error" href='#exercises'
+        sx={{ backgroundColor:"#ff2625", padding:"10px"}}>Explore Exercises</Button>
+        <Typography 
+        fontWeight={600}
+        color="#ff2625"
+        sx={{opacity:0.1,
+        display: {ig: "block", xs:"none"}
+        }}
+        fontSize="200px">
+            Exercises
+        </Typography>
         <img src={banner} alt="Banner"
         className='hero-banner-img'/>
 
