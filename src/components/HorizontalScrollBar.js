198725e22df5fbd0b589ed5ccb34655aed02878b
Alexander784 <alexandernyaga1999@gmail.com>
2024-03-23 23:02:29 +0300

    Set the arrow to scroll the bodyparts

--- src/components/HorizontalScrollBar.js
+++ src/components/HorizontalScrollBar.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { useContext } from 'react';
+import { Box, Typography } from '@mui/material';
 import BodyParts from './BodyPart';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import LeftArrowIcon from "../assets/icons/left-arrow.png";
+import RightArrowIcon from "../assets/icons/right-arrow.png";
 
-  
+// Function to set the leftArrow on click
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+// Function to set the RightArrow on click
+const RightArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollPrev()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
 const HorizontalScrollBar = ( { data, bodyPart, setBodyPart } ) => {
   return (
+    // Display the data Horizontally
     <ScrollMenu>
         {data.map((item) => 
          <Box 
